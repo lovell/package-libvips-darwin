91ac466bedcb527b2c3b811ea8b6c0d3dda4a146
Lovell Fuller <github@lovell.info>
2018-09-20 09:34:42 +0100

    Update homebrew package flags

--- package.sh
+++ package.sh
@@ -8,20 +8,17 @@ mkdir lib include
 for path in $(pkg-config --cflags --static vips-cpp libcroco-0.6 | tr ' ' '\n' | grep '^-I' | cut -c 3- | sort | uniq); do
   cp -R ${path}/ include;
 done;
-rm include/vips/vipsc++.h
 
 # Manually copy JPEG and GIF header files
-cp /usr/local/opt/jpeg-turbo/include/*.h include
+cp /usr/local/opt/jpeg/include/*.h include
 cp /usr/local/opt/giflib/include/*.h include
 
 # Use pkg-config to automagically find and copy necessary dylib files
 for path in $(pkg-config --libs --static vips-cpp libcroco-0.6 | tr ' ' '\n' | grep '^-L' | cut -c 3- | sort | uniq); do
   find ${path} -type f -name *.dylib | xargs -I {} cp {} lib;
 done;
-rm -f lib/libvipsCC.*.dylib
 
 # Manually copy JPEG and GIF dylib files
-cp /usr/local/opt/jpeg-turbo/lib/libjpeg.8.dylib lib
 cp /usr/local/opt/jpeg/lib/libjpeg.9.dylib lib
 cp /usr/local/opt/giflib/lib/libgif.7.dylib lib
 
@@ -47,6 +44,7 @@ printf "{\n\
   \"exif\": \"$(pkg-config --modversion libexif)\",\n\
   \"fontconfig\": \"$(pkg-config --modversion fontconfig)\",\n\
   \"freetype\": \"$(pkg-config --modversion freetype2)\",\n\
+  \"fribidi\": \"$(pkg-config --modversion fribidi)\",\n\
   \"gdkpixbuf\": \"$(pkg-config --modversion gdk-pixbuf-2.0)\",\n\
   \"gif\": \"$(grep GIFLIB_ include/gif_lib.h | cut -d' ' -f3 | paste -s -d'.' -)\",\n\
   \"glib\": \"$(pkg-config --modversion glib-2.0)\",\n\
