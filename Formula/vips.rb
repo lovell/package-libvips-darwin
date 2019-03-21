1ce865c80d5b2138bab021638025744a21877a38
Kleis Auke Wolthuizen <github@kleisauke.nl>
2019-03-21 14:37:26 +0100

    Prefer jpeg-turbo over jpeg
    It's 4x faster, better tested, and binary compatible.

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -16,7 +16,7 @@ class Vips < Formula
   depends_on "gettext"
   depends_on "giflib"
   depends_on "glib"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libexif"
   depends_on "libgsf"
   depends_on "libpng"
